43e58124b9ab05ecf418ffbcff968783428d09a4
HypoCycle <101015434+HypoCycle@users.noreply.github.com>
2022-10-10 17:37:23 +0100

    Create Ejercicio - 2.js

---
+++ Ejercicio - 2.js
@@ -0,0 +1,17 @@
+const nombre = "Pablo";
+const fecha = [1993, 12, 7];
+const desarrollador = Boolean;
+Boolean = true;
+
+
+const libro = {
+    titulo: "Los orígenes del hombre",
+    autor: "Colab.",
+    fecha_publicacion: (2000, 7, 24)
+
+}
+
+
+const lista = [nombre, fecha, libro, desarrollador]
+
+console.log(lista);

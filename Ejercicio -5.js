4c1a3b9581b7cd76ff3daa444eae333d351ea7d4
HypoCycle <101015434+HypoCycle@users.noreply.github.com>
2022-10-17 17:24:18 +0100

    Create Ejercicio -5.js

---
+++ Ejercicio -5.js
@@ -0,0 +1,16 @@
+let alturaCm = 179;
+let alturaM = 1.79;
+let pesoKg = 78.5;
+
+let alturaRedondeada = alturaM.toFixed();
+console.log(alturaRedondeada);
+
+let pesoRedondeado = Math.floor(pesoKg);
+console.log(pesoRedondeado);
+
+let max_valor_JavaScript = "el máximo valor que se puede obtener en Javascript + 1"
+
+
+let max = Number.MAX_VALUE;
+let max1 = Number.MAX_VALUE + 1;
+console.log(max1);
